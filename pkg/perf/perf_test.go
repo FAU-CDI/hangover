6fde63b6c544555dd321e8cf866a8c70e8767ad0
Tom Wiesing <tkw01536@gmail.com>
2023-09-11 13:52:06 +0200

    perf: Remove broken test

--- pkg/perf/perf_test.go
+++ pkg/perf/perf_test.go
@@ -2,42 +2,10 @@ package perf_test
 
 import (
 	"fmt"
-	"runtime"
 	"time"
 
 	"github.com/FAU-CDI/hangover/pkg/perf"
 )
-
-// An example of capturing performance metrics
-func ExampleNow() {
-	metrics := perf.Now()
-
-	const ARRAY_SIZE = 1000
-	const SLEEP = 1 * time.Second
-
-	// some fancy and slow task
-	{
-		var stuff [ARRAY_SIZE]int32
-		defer runtime.KeepAlive(stuff)
-		time.Sleep(SLEEP)
-	}
-
-	// capture the new metrics
-	diff := perf.Since(metrics)
-
-	// check that we slept long enough
-	if diff.Time >= SLEEP {
-		fmt.Println("a lot of time has passed")
-	}
-
-	// check that enough memory was allocated
-	if diff.Bytes >= 4*ARRAY_SIZE {
-		fmt.Println("a lot of memory was allocated")
-	}
-
-	// Output: a lot of time has passed
-	// a lot of memory was allocated
-}
 
 func ExampleDiff() {
 	// Diff holds both the amount of time an operation took,
